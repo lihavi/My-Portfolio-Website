83fdb6a605cc95f9bf166ccf6fc2443f1ce5b628
vivianne <viviannjoroge91@gmail.com>
2024-01-30 11:53:03 +0300

    Add styling changes

--- script.js
+++ script.js
@@ -1,17 +1,21 @@
 /* scroll sections */
-let sections =document.querySelectorAll('section');
+/* let sections =document.querySelectorAll('section');
 let navLinks = document.querySelectorAll('header nav a');
 
 window.onscroll = () => {
-    sections.forEach(section =>{
+    sections.forEach(sec =>{
         let top = window.scrollY;
         let offset = sec.offsetTop - 100;
         let height = sec.offsetHeight;
         let id = sec.getAttribute('id');
-     
 
-    
+        if(top>=offset&& top<=height )
+
+
+     
+    });
+};
     });
     let header = document.querySelector('header');
     header.classList.toggle('sticky',window.scrollY > 100);
-}
+ */
